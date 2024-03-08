f1ffb5689f4646cdbbb51bca6e71f89adaa68fff
한상희 <tkdgml822@gmail.com>
2024-03-08 12:58:18 +0900

    03월 08일 수업

---
+++ src/kr/ac/ync/computer/ex240308/Exam.java
@@ -0,0 +1,34 @@
+package kr.ac.ync.computer.ex240308;
+
+class Member {
+    int age;
+    String name;
+
+    public Member(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
+
+public class Exam {
+    public static int test(int x, int y) {
+        return (x > y) ? x : y;
+    }
+
+    public static void main(String[] args) {
+
+
+//        Member member = new Member(10, "Kim");
+//        System.out.println(member.age);
+//        Member member2 = member;
+//        member2.age = 20;
+//        System.out.println(member.age);
+
+//        int x = 5;
+//        int y = 100;
+//
+//        int result = (x > y)?x:y;
+//
+//        System.out.println(result);
+    }
+}
