d0d85cd4952396a2ea2e6efb2318591c57b93688
한상희 <tkdgml822@gmail.com>
2024-03-15 11:42:18 +0900

    0315일 수업

--- src/kr/ac/ync/computer/ex240308/Exam.java
+++ src/kr/ac/ync/computer/ex240308/Exam.java
@@ -1,34 +1,36 @@
-package kr.ac.ync.computer.ex240308;
-
-class Member {
-    int age;
-    String name;
-
-    public Member(int age, String name) {
-        this.age = age;
-        this.name = name;
-    }
-}
-
-public class Exam {
-    public static int test(int x, int y) {
-        return (x > y) ? x : y;
-    }
-
-    public static void main(String[] args) {
-
-
-//        Member member = new Member(10, "Kim");
-//        System.out.println(member.age);
-//        Member member2 = member;
-//        member2.age = 20;
-//        System.out.println(member.age);
-
-//        int x = 5;
-//        int y = 100;
+//package kr.ac.ync.computer.ex240308;
 //
-//        int result = (x > y)?x:y;
+//class Member {
+//    int age;
+//    String name;
 //
-//        System.out.println(result);
-    }
-}
+////    public Member(int age, String name) {
+////        this.age = age;
+////        this.name = name;
+////    }
+//}
+//
+//public class Exam {
+//    public static int test(int x, int y) {
+//        return (x > y) ? x : y;
+//    }
+//
+//    public static void main(String[] args) {
+//
+//
+////        Member member = new Member(10, "Kim");
+////        System.out.println(member.age);
+////        Member member2 = member;
+////        member2.age = 20;
+////        System.out.println(member.age);
+//
+////        int x = 5;
+////        int y = 100;
+////
+////        int result = (x > y)?x:y;
+////
+////        System.out.println(result);
+//
+//
+//    }
+//}
---
+++ src/kr/ac/ync/computer/ex240315/Exam.java
@@ -0,0 +1,59 @@
+package kr.ac.ync.computer.ex240315;
+class Member {
+    private int age;
+    private String name;
+
+//    public Member(int age, String name) {
+//        this.age = age;
+//        this.name = name;
+//    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
+public class Exam {
+    public static void main(String[] args) {
+
+        for (int i = 0; ; i++) {
+            if (i > 10) {
+                break;
+            }
+
+            if (i % 2 != 0) {
+                continue;
+            }
+
+            System.out.print(i + " ");
+        }
+
+
+//        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//        System.out.println(array[0] * 5);
+
+//        Member[] members; // 메소드 영역에 선언
+//        members = new Member[5]; // 그 메소드 영역에 5개의 배열을 선언
+//        members[0] = new Member(); // 그 다섯개의 배열에서 Member 선언
+//        members[0].age = 10;
+//        members[0].name = "한상희";
+
+//        System.out.println(members[0].age + " " +  members[0].name);
+
+        // 접근 지정자까지 진행
+    }
+}
